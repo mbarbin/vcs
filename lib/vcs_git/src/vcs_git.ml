d03926b750a219096f63904adf9ecb9754e54d29
Mathieu Barbin <mathieu.barbin@gmail.com>
2024-03-05 19:36:06 +0100

    Fmt

--- lib/vcs_git/src/vcs_git.ml
+++ lib/vcs_git/src/vcs_git.ml
@@ -18,4 +18,3 @@
 (*  and the LGPL-3.0 Linking Exception along with this library. If not, see    *)
 (*  <http://www.gnu.org/licenses/> and <https://spdx.org>, respectively.       *)
 (*******************************************************************************)
-
