2e58096acf217ec486c0938f8db6d665815df595
Mathieu Barbin <mathieu.barbin@gmail.com>
2024-03-11 14:54:37 +0100

    Import vcs-param library

--- lib/vcs_param/src/vcs_param.ml
+++ lib/vcs_param/src/vcs_param.ml
@@ -1,20 +1,188 @@
-(*******************************************************************************)
-(*  Vcs - a versatile OCaml library for Git interaction                        *)
-(*  Copyright (C) 2024 Mathieu Barbin <mathieu.barbin@gmail.com>               *)
-(*                                                                             *)
-(*  This file is part of Vcs.                                                  *)
-(*                                                                             *)
-(*  Vcs is free software; you can redistribute it and/or modify it under       *)
-(*  the terms of the GNU Lesser General Public License as published by the     *)
-(*  Free Software Foundation either version 3 of the License, or any later     *)
-(*  version, with the LGPL-3.0 Linking Exception.                              *)
-(*                                                                             *)
-(*  Vcs is distributed in the hope that it will be useful, but WITHOUT ANY     *)
-(*  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS  *)
-(*  FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License and    *)
-(*  the file `NOTICE.md` at the root of this repository for more details.      *)
-(*                                                                             *)
-(*  You should have received a copy of the GNU Lesser General Public License   *)
-(*  and the LGPL-3.0 Linking Exception along with this library. If not, see    *)
-(*  <http://www.gnu.org/licenses/> and <https://spdx.org>, respectively.       *)
-(*******************************************************************************)
+module Config = struct
+  (* This is boilerplate code to be used when we'll have things to select, such
+     as several backends, or backend modifiers. *)
+  type t = { unit : unit }
+
+  let _silence_w69_unused_field t =
+    ignore (t.unit : unit);
+    ()
+  ;;
+
+  let param =
+    let%map_open.Command () = return () in
+    { unit = () }
+  ;;
+end
+
+let config = Config.param
+
+module Create_vcs_backend = struct
+  let repo_root (dir : _ Eio.Path.t) =
+    dir
+    |> snd
+    |> Absolute_path.of_string
+    |> Or_error.ok_exn
+    |> Vcs.Repo_root.of_absolute_path
+  ;;
+
+  let from_cwd ~env ~cwd ~config:_ =
+    let fs = Eio.Stdenv.fs env in
+    match
+      With_return.with_return_option (fun { return } ->
+        let rec visit dir =
+          List.iter (Eio.Path.read_dir dir) ~f:(fun entry ->
+            let path = Eio.Path.(dir / entry) in
+            match entry with
+            | ".git" -> if Eio.Path.is_directory path then return (`Git, dir)
+            | _ -> ());
+          match Eio.Path.split dir with
+          | None -> ()
+          | Some (parent_dir, _) -> visit parent_dir
+        in
+        visit Eio.Path.(fs / (cwd |> Absolute_path.to_string)))
+    with
+    | None -> None
+    | Some ((`Git as vcs), dir) ->
+      let vcs =
+        match vcs with
+        | `Git -> Vcs_git.create ~env
+      in
+      let repo_root = repo_root dir in
+      Some (vcs, repo_root)
+  ;;
+end
+
+module Context = struct
+  type t =
+    { config : Config.t
+    ; fs : Eio.Fs.dir_ty Eio.Path.t
+    ; cwd : Absolute_path.t
+    ; vcs : Vcs_git.t'
+    ; repo_root : Vcs.Repo_root.t
+    }
+
+  let _silence_w69_unused_field t =
+    ignore (t.config : Config.t);
+    ignore (t.fs : Eio.Fs.dir_ty Eio.Path.t);
+    ()
+  ;;
+
+  let create ~env ~config =
+    let cwd = Unix.getcwd () |> Absolute_path.v in
+    let%bind vcs, repo_root =
+      match Create_vcs_backend.from_cwd ~env ~cwd ~config with
+      | Some x -> Ok x
+      | None -> Or_error.error_string "Not in a supported version control repo"
+    in
+    return
+      { config
+      ; fs = (Eio.Stdenv.fs env :> Eio.Fs.dir_ty Eio.Path.t)
+      ; cwd
+      ; vcs
+      ; repo_root
+      }
+  ;;
+end
+
+module Initialized = struct
+  type t =
+    { vcs : Vcs_git.t'
+    ; repo_root : Vcs.Repo_root.t
+    ; context : Context.t
+    }
+end
+
+let initialize ~env ~config =
+  let%bind c = Context.create ~env ~config in
+  return { Initialized.vcs = c.vcs; repo_root = c.repo_root; context = c }
+;;
+
+type 'a t = Context.t -> 'a Or_error.t
+
+let resolve t ~context = t context
+
+let anon_absolute_path =
+  let%map_open.Command path = anon ("file" %: string) in
+  Absolute_path.of_string path
+;;
+
+let anon_branch_name =
+  let%map_open.Command branch_name = anon ("branch" %: string) in
+  Vcs.Branch_name.of_string branch_name
+;;
+
+let anon_branch_name_opt =
+  let%map_open.Command branch_name = anon (maybe ("branch" %: string)) in
+  branch_name |> Option.map ~f:Vcs.Branch_name.of_string
+;;
+
+let anon_path =
+  let%map_open.Command path = anon ("file" %: string) in
+  fun (c : Context.t) ->
+    Or_error.try_with (fun () -> Absolute_path.relativize ~root:c.cwd (path |> Fpath.v))
+;;
+
+let anon_path_in_repo =
+  let%map_open.Command path = anon ("file" %: string) in
+  fun (c : Context.t) ->
+    let repo_root = Vcs.Repo_root.to_absolute_path c.repo_root in
+    Or_error.try_with_join (fun () ->
+      let path = Absolute_path.relativize ~root:c.cwd (path |> Fpath.v) in
+      let%bind relative_path = Absolute_path.chop_prefix ~prefix:repo_root path in
+      return (Vcs.Path_in_repo.of_relative_path relative_path))
+;;
+
+let anon_rev =
+  let%map_open.Command rev = anon ("rev" %: string) in
+  Vcs.Rev.of_string rev
+;;
+
+let below_path_in_repo =
+  let%map_open.Command path =
+    flag "--below" (optional string) ~doc:"PATH only below path"
+  in
+  fun (c : Context.t) ->
+    let repo_root = Vcs.Repo_root.to_absolute_path c.repo_root in
+    Or_error.try_with_join (fun () ->
+      match path with
+      | None -> return None
+      | Some path ->
+        let path = Absolute_path.relativize ~root:c.cwd (path |> Fpath.v) in
+        let%bind relative_path = Absolute_path.chop_prefix ~prefix:repo_root path in
+        return (Some (Vcs.Path_in_repo.of_relative_path relative_path)))
+;;
+
+let commit_message =
+  let%map_open.Command commit_message =
+    flag "--message" ~aliases:[ "-m" ] (required string) ~doc:"MSG commit message"
+  in
+  Vcs.Commit_message.of_string commit_message
+;;
+
+let quiet =
+  let%map_open.Command quiet =
+    flag "--quiet" ~aliases:[ "-q" ] no_arg ~doc:" suppress output on success"
+  in
+  quiet
+;;
+
+let rev =
+  let%map_open.Command rev =
+    flag "--rev" ~aliases:[ "-r" ] (required string) ~doc:"REV revision"
+  in
+  Vcs.Rev.of_string rev
+;;
+
+let user_name =
+  let%map_open.Command user_name =
+    flag "--user.name" (required string) ~doc:"USER user name"
+  in
+  Vcs.User_name.of_string user_name
+;;
+
+let user_email =
+  let%map_open.Command user_email =
+    flag "--user.email" (required string) ~doc:"EMAIL user email"
+  in
+  Vcs.User_email.of_string user_email
+;;
--- lib/vcs_param/src/vcs_param.mli
+++ lib/vcs_param/src/vcs_param.mli
@@ -1,20 +1,79 @@
-(*_******************************************************************************)
-(*_  Vcs - a versatile OCaml library for Git interaction                        *)
-(*_  Copyright (C) 2024 Mathieu Barbin <mathieu.barbin@gmail.com>               *)
-(*_                                                                             *)
-(*_  This file is part of Vcs.                                                  *)
-(*_                                                                             *)
-(*_  Vcs is free software; you can redistribute it and/or modify it under       *)
-(*_  the terms of the GNU Lesser General Public License as published by the     *)
-(*_  Free Software Foundation either version 3 of the License, or any later     *)
-(*_  version, with the LGPL-3.0 Linking Exception.                              *)
-(*_                                                                             *)
-(*_  Vcs is distributed in the hope that it will be useful, but WITHOUT ANY     *)
-(*_  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS  *)
-(*_  FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License and    *)
-(*_  the file `NOTICE.md` at the root of this repository for more details.      *)
-(*_                                                                             *)
-(*_  You should have received a copy of the GNU Lesser General Public License   *)
-(*_  and the LGPL-3.0 Linking Exception along with this library. If not, see    *)
-(*_  <http://www.gnu.org/licenses/> and <https://spdx.org>, respectively.       *)
-(*_******************************************************************************)
+(** Common command line parameters in use in vcs. *)
+
+(** {1 Initialization} *)
+
+module Config : sig
+  type t
+end
+
+val config : Config.t Command.Param.t
+
+module Context : sig
+  type t
+end
+
+module Initialized : sig
+  type t =
+    { vcs : Vcs_git.t'
+    ; repo_root : Vcs.Repo_root.t
+    ; context : Context.t
+    }
+end
+
+(** The initialization should be created very early in the commands body. The
+    [cwd] shall not be changed subsequently. *)
+val initialize
+  :  env:< fs : _ Eio.Path.t ; process_mgr : _ Eio.Process.mgr ; .. >
+  -> config:Config.t
+  -> Initialized.t Or_error.t
+
+(** {1 Params}
+
+    Some parameters may only be created under a certain context, in that case
+    they're exposed wrapped under a resolvable type. Otherwise they can be
+    exposed as command params directly.
+
+    If not otherwise specified, the parameters are resolved under the current
+    context, and are required. Optional parameters ends with [_optional]. *)
+
+(** A ['a Vcs_param.t] is a command line parameter that produces a value of type
+    ['a] under the initialized context. *)
+type 'a t
+
+(** To be called in the body of the command, after initialization. *)
+val resolve : 'a t -> context:Context.t -> 'a Or_error.t
+
+(** An anonymous parameter for an absolute path. *)
+val anon_absolute_path : Absolute_path.t Or_error.t Command.Param.t
+
+(** A required anon [BRANCH]. *)
+val anon_branch_name : Vcs.Branch_name.t Or_error.t Command.Param.t
+
+(** An optional anon [BRANCH]. *)
+val anon_branch_name_opt : Vcs.Branch_name.t Or_error.t option Command.Param.t
+
+(** An anonymous parameter for a path. It can be given either as an absolute
+    path or relative path in the command line, but will always be resolved to an
+    absolute path. *)
+val anon_path : Absolute_path.t t Command.Param.t
+
+(** An anonymous parameter for a path in repo. *)
+val anon_path_in_repo : Vcs.Path_in_repo.t t Command.Param.t
+
+(** A required anon [REV]. *)
+val anon_rev : Vcs.Rev.t Or_error.t Command.Param.t
+
+(** A flag to restrict the repo to a subdirectory below a certain path. *)
+val below_path_in_repo : Vcs.Path_in_repo.t option t Command.Param.t
+
+(** A required [-m _] nonempty commit message. *)
+val commit_message : Vcs.Commit_message.t Or_error.t Command.Param.t
+
+(** Perform the side effect if any, but suppress the output in case of success. *)
+val quiet : bool Command.Param.t
+
+(** A required [--rev _] that produces a revision. *)
+val rev : Vcs.Rev.t Or_error.t Command.Param.t
+
+val user_email : Vcs.User_email.t Or_error.t Command.Param.t
+val user_name : Vcs.User_name.t Or_error.t Command.Param.t
