b0234ca04097650922aecd014dc183d99c329a49
Mathieu Barbin <mathieu.barbin@gmail.com>
2024-07-24 12:06:03 +0200

    Make sidebar hideable

--- doc/docusaurus.config.ts
+++ doc/docusaurus.config.ts
@@ -97,6 +97,12 @@ const config: Config = {
         },
       ],
     },
+    docs: {
+      sidebar: {
+        hideable: true,
+        autoCollapseCategories: true,
+      },
+    },
     footer: {
       style: 'dark',
       links: [
