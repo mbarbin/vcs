536ca62b8e19681c1948611fb93bb626d4b252fe
Mathieu Barbin <mathieu.barbin@gmail.com>
2024-07-25 21:27:33 +0200

    Enable DocSearch with Algolia

--- doc/docusaurus.config.ts
+++ doc/docusaurus.config.ts
@@ -144,6 +144,33 @@ const config: Config = {
       darkTheme: prismThemes.dracula,
       additionalLanguages: ['bash', 'diff', 'json', 'ocaml'],
     },
+    algolia: {
+      // The application ID provided by Algolia
+      appId: 'SK93BEXKXL',
+      // Public API key: it is safe to commit it
+      apiKey: '3696b679996f24295fa2f4a80bbc366b',
+      indexName: 'mbarbinio',
+      // Optional: see doc section below
+      contextualSearch: true,
+      // Optional: Specify domains where the navigation should occur through
+      // window.location instead on history.push. Useful when our Algolia
+      // config crawls multiple documentation sites and we want to navigate
+      // with window.location.href to them.
+      // externalUrlRegex: 'external\\.com|domain\\.com',
+      // Optional: Replace parts of the item URLs from Algolia. Useful when
+      // using the same search index for multiple deployments using a
+      // different baseUrl. You can use regexp or string in the `from` param.
+      // For example: localhost:3000 vs myCompany.com/docs
+      // replaceSearchResultPathname: {
+      //  from: '/docs/', // or as RegExp: /\/docs\//
+      //  to: '/',
+      // },
+      // Optional: Algolia search parameters
+      searchParameters: {},
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      searchPagePath: 'search',
+      //... other Algolia params
+    },
   } satisfies Preset.ThemeConfig,
 };
 
