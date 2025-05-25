410dc8d92d7da21482f35f6535cf36a372994a62
Mathieu Barbin <mathieu.barbin@gmail.com>
2025-05-25 22:21:17 +0200

    Move directories according to libraries

--- bin/main.ml
+++ bin/main.ml
@@ -20,5 +20,5 @@
 (*******************************************************************************)
 
 let () =
-  Cmdlang_cmdliner_runner.run Volgo_cli.main ~name:"volgo-vcs" ~version:"%%VERSION%%"
+  Cmdlang_cmdliner_runner.run Volgo_vcs_cli.main ~name:"volgo-vcs" ~version:"%%VERSION%%"
 ;;
