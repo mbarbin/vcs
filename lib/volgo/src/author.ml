d2699098417aa8f85306122cdce0ee7338d0a913
Mathieu Barbin <mathieu.barbin@gmail.com>
2025-06-30 21:04:14 +0200

    Test CR

--- lib/volgo/src/author.ml
+++ lib/volgo/src/author.ml
@@ -19,6 +19,8 @@
 (*  <http://www.gnu.org/licenses/> and <https://spdx.org>, respectively.       *)
 (*******************************************************************************)
 
+(* CR mbarbin: Hello CR! *)
+
 open! Import
 include Container_key.String_impl
 
