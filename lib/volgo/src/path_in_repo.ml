3c6b1f7bf32441c0568739dba020c2e3d2d7591d
Mathieu Barbin <mathieu.barbin@gmail.com>
2025-10-24 23:34:46 +0200

    Upgrade to ocamlformat.0.28.1

--- lib/volgo/src/path_in_repo.ml
+++ lib/volgo/src/path_in_repo.ml
@@ -20,28 +20,28 @@
 (*******************************************************************************)
 
 (* This module is partially derived from Iron (v0.9.114.44+47), file
-   * [./common/path_in_repo.ml], which is released under Apache 2.0:
-   *
-   * Copyright (c) 2016-2017 Jane Street Group, LLC <opensource-contacts@janestreet.com>
-   *
-   * Licensed under the Apache License, Version 2.0 (the "License"); you may not
-   * use this file except in compliance with the License. You may obtain a copy
-   * of the License at:
-   *
-   *     http://www.apache.org/licenses/LICENSE-2.0
-   *
-   * Unless required by applicable law or agreed to in writing, software
-   * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-   * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-   * License for the specific language governing permissions and limitations
-   * under the License.
-   *
-   * See the file `NOTICE.md` at the root of this repository for more details.
-   *
-   * Changes: We removed the part related to the [Stable] serialization. The logic
-   * is based on a different module for [Relative_path]. We removed the part that
-   * related to [.fe] files.
-*)
+ * [./common/path_in_repo.ml], which is released under Apache 2.0:
+ *
+ * Copyright (c) 2016-2017 Jane Street Group, LLC <opensource-contacts@janestreet.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * See the file `NOTICE.md` at the root of this repository for more details.
+ *
+ * Changes: We removed the part related to the [Stable] serialization. The logic
+ * is based on a different module for [Relative_path]. We removed the part that
+ * related to [.fe] files.
+ *)
 
 open! Import
 include Relative_path
--- lib/volgo/src/path_in_repo.mli
+++ lib/volgo/src/path_in_repo.mli
@@ -20,27 +20,27 @@
 (*_******************************************************************************)
 
 (*_ This module is partially derived from Iron (v0.9.114.44+47), file
-  * [./common/path_in_repo.mli], which is released under Apache 2.0:
-  *
-  * Copyright (c) 2016-2017 Jane Street Group, LLC <opensource-contacts@janestreet.com>
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
-  * use this file except in compliance with the License. You may obtain a copy
-  * of the License at:
-  *
-  *     http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-  * License for the specific language governing permissions and limitations
-  * under the License.
-  *
-  * See the file `NOTICE.md` at the root of this repository for more details.
-  *
-  * The file [path_in_repo.ml] contains a detailed description of the changes made
-  * to the original file.
-*)
+ * [./common/path_in_repo.mli], which is released under Apache 2.0:
+ *
+ * Copyright (c) 2016-2017 Jane Street Group, LLC <opensource-contacts@janestreet.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * See the file `NOTICE.md` at the root of this repository for more details.
+ *
+ * The file [path_in_repo.ml] contains a detailed description of the changes made
+ * to the original file.
+ *)
 
 (** A path for a file versioned in a repository.
 
