20502344faf00b4acb8afbacb2b94ba83772da65
Mathieu Barbin <mathieu.barbin@gmail.com>
2026-03-24 10:00:10 +0100

    Use dune-build-info for --version

--- src/volgo-vcs/bin/main.ml
+++ src/volgo-vcs/bin/main.ml
@@ -4,9 +4,10 @@
 (*  SPDX-License-Identifier: LGPL-3.0-or-later WITH LGPL-3.0-linking-exception   *)
 (*********************************************************************************)
 
-let () =
-  Cmdlang_cmdliner_err_runner.run
-    Volgo_vcs_cli.main
-    ~name:"volgo-vcs"
-    ~version:"%%VERSION%%"
+let version =
+  match Build_info.V1.version () with
+  | None -> "n/a"
+  | Some v -> Build_info.V1.Version.to_string v
 ;;
+
+let () = Cmdlang_cmdliner_err_runner.run Volgo_vcs_cli.main ~name:"volgo-vcs" ~version
